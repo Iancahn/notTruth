d60fcc333cb2cd6d108b7af00d9f07b5fbbb35f8
Ian Cahn <iancahn@live.com>
2022-05-20 11:32:23 +0200

    Adding CreateTweet Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Tweet from "./components/Tweet";
+import CreateTweet from "./components/CreateTweet";
 
 function App() {
   return (
     <div className="App">
+      <CreateTweet />
       <Tweet />
     </div>
   );
---
+++ src/components/CreateTweet.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CreateTweet = () => {
+    return (
+        <div>
+            <form>
+                <textarea cols="50" rows="5">
+
+                </textarea>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default CreateTweet;
